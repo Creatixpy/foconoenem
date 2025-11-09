5e20f7d1f63da85a25de942720f54c48b1790efa
Gabriel <creatixpy@gmail.com>
2025-11-08 22:53:24 -0300

    Fix Postgrest builder generics

--- types/postgrest-augment.d.ts
+++ types/postgrest-augment.d.ts
@@ -1,12 +1,15 @@
 import '@supabase/postgrest-js';
 
 declare module '@supabase/postgrest-js' {
-  interface PostgrestBuilder<Row, Result, Relationships> {
+  interface PostgrestBuilder<Row = any, Result = Row[], Relationships = Record<string, unknown>> {
     abortSignal(signal: AbortSignal): PostgrestBuilder<Row, Result, Relationships>;
-    single(): PostgrestBuilder<Row, Result, Relationships>;
-    maybeSingle(): PostgrestBuilder<Row, Result, Relationships>;
+    single(): PostgrestBuilder<Row, Row, Relationships>;
+    maybeSingle(): PostgrestBuilder<Row | null, Row | null, Relationships>;
     limit(count: number, options?: { foreignTable?: keyof Relationships }): PostgrestBuilder<Row, Result, Relationships>;
     eq(column: string, value: unknown): PostgrestBuilder<Row, Result, Relationships>;
-    order(column: string, options?: { ascending?: boolean; nullsFirst?: boolean; referencedTable?: keyof Relationships }): PostgrestBuilder<Row, Result, Relationships>;
+    order(
+      column: string,
+      options?: { ascending?: boolean; nullsFirst?: boolean; referencedTable?: keyof Relationships }
+    ): PostgrestBuilder<Row, Result, Relationships>;
   }
 }
